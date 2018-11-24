128745f909f4a1463e26c9155590938d924f1235
Haley Rogers <hxr6344@g.rit.edu>
2018-11-24 18:45:05 -0500

    Uploaded character/monster sketches, create a simple UI for start menu and case file menus

--- Assets/Scripts/UIMngr.cs
+++ Assets/Scripts/UIMngr.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using UnityStandardAssets.Characters.FirstPerson;
 ///<summary> Handles all the toggling of menus </summary>
 public class UIMngr : MonoBehaviour
 {
@@ -39,30 +39,77 @@ public class UIMngr : MonoBehaviour
     #endregion
 
     public GameObject hud;
-    public GameObject options;
-    public GameObject overlay;
-    public GameObject mainMenu;
+    //public GameObject options;
+    //public GameObject overlay;   
     public GameObject caseFile;
-    public GameObject winScreen;
-    public GameObject deskTexture;
+    //public GameObject winScreen;
+    //public GameObject deskTexture;
+    public GameObject mainMenu;
+    public GameObject inventory;
+    public GameObject profile;
+    public GameObject bestiary;
 
     //public ElementDictionary elDic;
     //public SpellSOList spellCheckmarks;
     private bool audioOn;
     private GameObject previous;
-    
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
     {
         mainMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        //Disable the FPS to enable the cursor for GUI
+        GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
         hud.SetActive(false);
-        options.SetActive(false);
+        //options.SetActive(false);
         caseFile.SetActive(false);
-        winScreen.SetActive(false);
+        //winScreen.SetActive(false);
         audioOn = true;
     }
-
+    //check for keypress "c" to open and close the casefile
+    public void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            caseFile.SetActive(!caseFile.active);
+            hud.SetActive(!caseFile.active);
+            if (caseFile.active)
+            {
+                GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
+                Cursor.lockState = CursorLockMode.None;
+                //Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+            }
+            else
+            {
+                GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = true;
+            }
+        }
+    }
+    //go between profile, inventory and bestiary
+    public void ToggleFile(GameObject file)
+    {
+        file.SetActive(true);
+        if (file.name == "ProfilePanel")
+        {
+            inventory.SetActive(false);
+            bestiary.SetActive(false);
+        }
+        else if(file.name == "InventoryPanel")
+        {
+            profile.SetActive(false);
+            bestiary.SetActive(false);
+        }
+        else
+        {
+            inventory.SetActive(false);
+            profile.SetActive(false);
+        }
+    }
     //Sets the previous menu visited
     public void SetPrevious(GameObject prev)
     {
@@ -104,11 +151,12 @@ public class UIMngr : MonoBehaviour
     {
         //Toggle the HUD
         hud.SetActive(true);
-        options.SetActive(false);
+        //options.SetActive(false);
         mainMenu.SetActive(false);
-
+        //Enable the mouse to be locked
+        GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = true;
         // get reference to music manager and switch music loop
-        
+
     }
 
     public void ToggleCredits(GameObject credits)
@@ -130,28 +178,28 @@ public class UIMngr : MonoBehaviour
     //Toggles the options menu
     public void ToggleOptions()
     {
-        if (hud.active)
-        {
-            hud.SetActive(false);
-            options.SetActive(true);
-            overlay.SetActive(true);
-        }
-        else
-        {
-            hud.SetActive(true);
-            options.SetActive(false);
-            overlay.SetActive(false);
-        }
+        //if (hud.active)
+        //{
+        //    hud.SetActive(false);
+        //    options.SetActive(true);
+        //    overlay.SetActive(true);
+        //}
+        //else
+        //{
+        //    hud.SetActive(true);
+        //    options.SetActive(false);
+        //    overlay.SetActive(false);
+        //}
     }
 
     public void ToggleWinScreen()
     {
-        if(hud.activeSelf)
-        {
-            hud.SetActive(false);
-            deskTexture.SetActive(false);
-            winScreen.SetActive(true);
-        }
+        //if(hud.activeSelf)
+        //{
+        //    hud.SetActive(false);
+        //    deskTexture.SetActive(false);
+        //    winScreen.SetActive(true);
+        //}
     }
 
     public void ClearBoard()
