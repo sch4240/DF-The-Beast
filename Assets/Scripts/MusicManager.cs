3e2260367c210c3b2e8708a452df23d65d86ab0a
sharnold52 <31418741+sharnold52@users.noreply.github.com>
2018-11-26 13:58:48 -0500

    Pretty much finished implementing the sound system, but found out there is a problem with one of the exported music files. Seems I didn't export it as a loop or something, so I am going to fix that tomorrow

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -16,11 +16,21 @@ public class MusicManager : MonoBehaviour
     // the audio source
     public AudioSource[] sources;
 
+    // current audio source
+    private AudioSource currentAudio;
+
     // Use this for initialization
     void Start()
     {
         // start the audio
         StartZero();
+    }
+
+    // Update our music
+    void Update()
+    {
+        // slowly decrease audio of current audio source
+        currentAudio.volume -= 0.01f * Time.deltaTime;
     }
 
 
@@ -37,18 +47,28 @@ public class MusicManager : MonoBehaviour
         float nextTrack = Mathf.Floor(Random.Range(0, 1));
 
         // set source0 to active and rest to inactive
-        sources[0].enabled = true;
-        sources[1].enabled = false;
-        sources[2].enabled = false;
+        for (int i = 0; i < sources.Length; i++)
+        {
+            if(i == 0)
+            {
+                sources[i].enabled = true;
+                currentAudio = sources[i];
+                currentAudio.volume = 1.0f;
+            }
+            else
+            {
+                sources[i].enabled = false;
+            }
+        }
 
         // Pick next track, wait for two cycles, then play next track
         if (nextTrack == 0)
         {
-            Invoke("StartOne", bg0.length * 2.0f);
+            Invoke("StartOne", bg0.length * 5.0f);
         }
         else if (nextTrack == 1)
         {
-            Invoke("StartTwo", bg0.length * 2.0f);
+            Invoke("StartTwo", bg0.length * 5.0f);
         }
     }
 
@@ -58,19 +78,28 @@ public class MusicManager : MonoBehaviour
         // generate random number to decide what track to play next (0 - 1)
         float nextTrack = Mathf.Floor(Random.Range(0, 1));
 
-        // set source0 to active and rest to inactive
-        sources[0].enabled = false;
-        sources[1].enabled = true;
-        sources[2].enabled = false;
+        // set source1 to active and rest to inactive
+        for (int i = 0; i < sources.Length; i++)
+        {
+            if (i == 1)
+            {
+                sources[i].enabled = true;
+                currentAudio = sources[i];
+            }
+            else
+            {
+                sources[i].enabled = false;
+            }
+        }
 
         // Pick next track, wait for two cycles, then play next track
         if (nextTrack == 0)
         {
-            Invoke("StartZero", bg1.length * 2.0f);
+            Invoke("StartZero", bg1.length * 5.0f);
         }
         else if (nextTrack == 1)
         {
-            Invoke("StartTwo", bg1.length * 2.0f);
+            Invoke("StartTwo", bg1.length * 5.0f);
         }
     }
 
@@ -80,19 +109,28 @@ public class MusicManager : MonoBehaviour
         // generate random number to decide what track to play next (0 - 1)
         float nextTrack = Mathf.Floor(Random.Range(0, 1));
 
-        // set source0 to active and rest to inactive
-        sources[0].enabled = false;
-        sources[1].enabled = false;
-        sources[2].enabled = true;
+        // set source2 to active and rest to inactive
+        for (int i = 0; i < sources.Length; i++)
+        {
+            if (i == 2)
+            {
+                sources[i].enabled = true;
+                currentAudio = sources[i];
+            }
+            else
+            {
+                sources[i].enabled = false;
+            }
+        }
 
         // Pick next track, wait for two cycles, then play next track
         if (nextTrack == 0)
         {
-            Invoke("StartOne", bg2.length * 2.0f);
+            Invoke("StartOne", bg2.length * 5.0f);
         }
         else if (nextTrack == 1)
         {
-            Invoke("StartZero", bg2.length * 2.0f);
+            Invoke("StartZero", bg2.length * 5.0f);
         }
     }
 }
