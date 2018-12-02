9fe2d4cbfdf419f64e5d08bd4516b94414fd9d01
sch4240 <sch4240@IGM2025-14>
2018-12-02 17:55:35 -0500

    Animations for doors Progress

--- Assets/Scripts/AnimScript.cs
+++ Assets/Scripts/AnimScript.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Animator anim;
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.O)==true)
+        {
+            anim.SetTrigger("openingDoor");
+            anim.SetBool("doorClosed", false);
+            anim.SetBool("doorOpen", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C) == true)
+        {
+            anim.SetTrigger("closingDoor");
+            anim.SetBool("doorClosed", true);
+            anim.SetBool("doorOpen", false);
+        }
+    }
+
 }
