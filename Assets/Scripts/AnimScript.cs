9c1c6e6512a1f7c0b8d5b208b55e9d3dff70fac5
sch4240 <sch4240@g.rit.edu>
2018-12-05 10:53:36 -0500

    Door Bedroom Animations finished

--- Assets/Scripts/AnimScript.cs
+++ Assets/Scripts/AnimScript.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class AnimScript : MonoBehaviour {
     private Animation anim;
+    private bool ifOpen;
     // Use this for initialization
     void Start ()
     {
         anim = GetComponent<Animation>();
+        ifOpen = true;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.O) == true)
+        if (Input.GetKeyDown(KeyCode.O) == true && ifOpen == false)
         {
-            anim.Play();
+            anim.Play("Door Open");
+            ifOpen = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.C) == true)
+        if (Input.GetKeyDown(KeyCode.C) == true && ifOpen==true)
         {
-
+            anim.Play("Door Anim");
+            ifOpen = false;
         }
     }
 
