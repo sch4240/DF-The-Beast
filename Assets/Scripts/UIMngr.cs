53f11f610eb71572965a41aa71d7fdb8a6b894e4
hxr6344 <hxr6344@g.rit.edu>
2018-12-05 21:08:44 -0500

    Working on UI: create a warning panel for leaving, and made toggle features for the decision panel created for the win condition. Setting up logic for modifying the monster photo at the end

--- Assets/Scripts/UIMngr.cs
+++ Assets/Scripts/UIMngr.cs
@@ -42,13 +42,20 @@ public class UIMngr : MonoBehaviour
     //public GameObject options;
     //public GameObject overlay;   
     public GameObject caseFile;
-    //public GameObject winScreen;
+    public GameObject winScreen;
+    public GameObject decisionScreen;
+    public GameObject leavePanel;
     //public GameObject deskTexture;
     public GameObject mainMenu;
     public GameObject inventory;
     public GameObject profile;
     public GameObject bestiary;
-
+    public GameObject decisionChoice;
+    public GameObject decisionInventory;
+    public GameObject decisionBestiary;
+    public Dictionary<int, string> beastDictionary;
+    public bool[] beasts;
+    public int pickedBeast;
     //public ElementDictionary elDic;
     //public SpellSOList spellCheckmarks;
     private bool audioOn;
@@ -68,8 +75,11 @@ public class UIMngr : MonoBehaviour
         hud.SetActive(false);
         //options.SetActive(false);
         caseFile.SetActive(false);
-        //winScreen.SetActive(false);
+        winScreen.SetActive(false);
+        decisionScreen.SetActive(false);
         audioOn = true;
+        beasts = new bool[7];
+        setDictionary();
     }
     //check for keypress "c" to open and close the casefile
     public void Update()
@@ -105,10 +115,25 @@ public class UIMngr : MonoBehaviour
             profile.SetActive(false);
             bestiary.SetActive(false);
         }
-        else
+        else if(file.name == "BestiaryPanel")
         {
             inventory.SetActive(false);
             profile.SetActive(false);
+        }
+        else if (file.name == "DecisionChoice")
+        {
+            decisionInventory.SetActive(false);
+            decisionBestiary.SetActive(false);
+        }
+        else if (file.name == "DecisionInventory")
+        {
+            decisionChoice.SetActive(false);
+            decisionBestiary.SetActive(false);
+        }
+        else if (file.name == "DecisionBestiary")
+        {
+            decisionInventory.SetActive(false);
+            decisionChoice.SetActive(false);
         }
     }
     //Sets the previous menu visited
@@ -174,7 +199,92 @@ public class UIMngr : MonoBehaviour
         }
     }
     #endregion
+    //Based if the user clicks the yes or no button to leave
+    //if yes, enable decisionPanel
+    public void LeaveDecision(string decision)
+    {
+        leavePanel.SetActive(false);
+        //when the door is interactive remove this line of code
+        GameObject.FindWithTag("FPSController").GetComponent<FirstPersonController>().enabled = false;
+        if (decision == "Yes")
+        {
+            decisionScreen.SetActive(true);
+            hud.SetActive(false);
+        }
+        else
+        {
+            GameObject.FindWithTag("FPSController").GetComponent<FirstPersonController>().enabled = true;
+        }
+    }
+    //If the user clicks on the door to leave, pull up the leaving panel
+    public void ToggleLeaving()
+    {
+        leavePanel.SetActive(true);
+        GameObject.FindWithTag("FPSController").GetComponent<FirstPersonController>().enabled = false;
+    }
+    //which beast did the user pick? Modify the bool array based on button press
+    public void setBeast(int num)
+    {
+        //set the rest to be false
+        for (int i = 0; i < 7; i++)
+        {
+            beasts[i] = false;
+        }
+        beasts[num] = true;
+    }
+    //makes the player have to pick a monster before the win panel
+    public void enableConfirm()
+    {
+        GameObject.Find("ConfirmationButton").GetComponent<Button>().interactable = true;
+    }
+    //based on the beasts bool array if true, return the string of the monster
+    public string getBeast()
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            if (beasts[j] == true)
+            {
+                pickedBeast = j;
+                return beastDictionary[j];
 
+            }
+        }
+        return null;
+    }
+    //set up a dictionary of all the possible beasts, to get the image for win panel
+    public void setDictionary()
+    {
+        beastDictionary = new Dictionary<int, string>();
+        beastDictionary.Add(0, "whiteVampireColor");
+        beastDictionary.Add(1, "redVampireColor");
+        beastDictionary.Add(2, "fetchColor");
+        beastDictionary.Add(3, "grendelkinColor");
+        beastDictionary.Add(4, "rawheadColor");
+        beastDictionary.Add(5, "skinwalkerColor");
+        beastDictionary.Add(6, "oldOneColor");
+    }
+    public void toggleDecisionPanel()
+    {
+        changeImage();
+        decisionScreen.SetActive(false);
+        winScreen.SetActive(true);
+    }
+    //modify the texture to the chosen monster's image
+    //not working at the moment
+    public void changeImage()
+    {
+        string name = getBeast();
+        //if (name != null)
+        //{
+        //    Texture changedTexture = (Texture)Resources.Load(name, typeof(Texture));
+        //    if(changedTexture !=null)
+        //    {
+        //        GameObject.Find("PickedMonster").GetComponent<RawImage>().texture = changedTexture;
+        //    }
+
+        //}
+     
+    }
     #region Options Menu
     //Toggles the options menu
     public void ToggleOptions()
