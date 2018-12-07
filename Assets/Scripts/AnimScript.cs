5a0c74704fd841f0b38ab94a7e98e05be474fa68
sharnold52 <31418741+sharnold52@users.noreply.github.com>
2018-12-07 14:13:31 -0500

    Doors Open when looking at them and you press F

--- Assets/Scripts/AnimScript.cs
+++ Assets/Scripts/AnimScript.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class AnimScript : MonoBehaviour {
     private Animation anim;
-    private bool ifOpen;
+    public bool ifOpen;
+    public float interactDistance = 5.0f;
+
+    // name of animation
+    public string animationOpenName;
+    public string animationCloseName;
 
     // sound effect variables
     SoundEffectManager SEManager;
@@ -22,11 +27,23 @@ public class AnimScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.O) == true && ifOpen == false)
         {
-            anim.Play("Door Open");
-            anim.Play("BathOpen");
-            anim.Play("CellarOpen");
+            // create raycast
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            // check if looking at door
+            if(Physics.Raycast(ray, out hit, interactDistance))
+            {
+                Debug.Log("We here?");
+                if(hit.collider.CompareTag("Door"))
+                {
+                    hit.collider.GetComponent<AnimScript>().OpenDoor();
+                }
+            }
+
             ifOpen = true;
 
             // play sound effect
@@ -35,14 +52,43 @@ public class AnimScript : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.C) == true && ifOpen==true)
         {
-            anim.Play("Door Anim");
-            anim.Play("BathClose");
-            anim.Play("CellarClose");
+            // create raycast
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            // check if looking at door
+            if (Physics.Raycast(ray, out hit, interactDistance))
+            {
+                if (hit.collider.CompareTag("Door"))
+                {
+                    hit.collider.GetComponent<AnimScript>().CloseDoor();
+                }
+            }
+
             ifOpen = false;
 
             // play sound effect
             SEManager.PlayCreak();
         }
+        */
     }
 
+
+    public void OpenDoor()
+    {
+        anim.Play(animationOpenName);
+        ifOpen = true;
+
+        // play sound effect
+        SEManager.PlayCreak();
+    }
+
+    public void CloseDoor()
+    {
+        anim.Play(animationCloseName);
+        ifOpen = false;
+
+        // play sound effect
+        SEManager.PlayCreak();
+    }
 }
---
+++ Assets/Scripts/DoorInteract.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteract : MonoBehaviour
+{
+
+    public float interactDistance;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // open/close door on F
+        if(Input.GetKeyDown(KeyCode.F) == true)
+        {
+            // create raycast
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, interactDistance))
+            {
+                if(hit.collider.CompareTag("Door"))
+                {
+                    if(hit.collider.GetComponent<AnimScript>().ifOpen)
+                    {
+                        hit.collider.GetComponent<AnimScript>().CloseDoor();
+                    }
+                    else
+                    {
+                        hit.collider.GetComponent<AnimScript>().OpenDoor();
+                    }
+                }
+            }
+        }
+    }
+}
