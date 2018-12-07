78f2e59057b273911fdf69ec3d56778270164cd6
sharnold52 <31418741+sharnold52@users.noreply.github.com>
2018-12-07 15:47:43 -0500

    AnimScript cleanup

--- Assets/Scripts/AnimScript.cs
+++ Assets/Scripts/AnimScript.cs
@@ -23,55 +23,6 @@ public class AnimScript : MonoBehaviour {
         // get reference to Sound Effect Manager
         SEManager = GameObject.FindObjectOfType<SoundEffectManager>();
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        /*
-        if (Input.GetKeyDown(KeyCode.O) == true && ifOpen == false)
-        {
-            // create raycast
-            Ray ray = new Ray(transform.position, transform.forward);
-            RaycastHit hit;
-
-            // check if looking at door
-            if(Physics.Raycast(ray, out hit, interactDistance))
-            {
-                Debug.Log("We here?");
-                if(hit.collider.CompareTag("Door"))
-                {
-                    hit.collider.GetComponent<AnimScript>().OpenDoor();
-                }
-            }
-
-            ifOpen = true;
-
-            // play sound effect
-            SEManager.PlayCreak();
-        }
-
-        if (Input.GetKeyDown(KeyCode.C) == true && ifOpen==true)
-        {
-            // create raycast
-            Ray ray = new Ray(transform.position, transform.forward);
-            RaycastHit hit;
-
-            // check if looking at door
-            if (Physics.Raycast(ray, out hit, interactDistance))
-            {
-                if (hit.collider.CompareTag("Door"))
-                {
-                    hit.collider.GetComponent<AnimScript>().CloseDoor();
-                }
-            }
-
-            ifOpen = false;
-
-            // play sound effect
-            SEManager.PlayCreak();
-        }
-        */
-    }
 
 
     public void OpenDoor()
